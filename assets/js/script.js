9c733648a3caecac5db7263072edd193aa0b657a
Gavin Jacobsen <gjacobsen1331@gmail.com>
2022-02-21 18:20:39 -0800

    add dropdown and task name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+// when button is clicked fuction is called to create new task-item
 var createTaskHandler = function() {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -8,4 +9,7 @@ var createTaskHandler = function() {
   tasksToDoEl.appendChild(listItemEl);
 }
 
+// on button click create task
 buttonEl.addEventListener("click", createTaskHandler);
+
+// Lesson 4.2.5

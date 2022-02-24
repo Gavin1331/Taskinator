8e5c13cd05030d169b8e7b82d566484e819225c3
Gavin Jacobsen <gjacobsen1331@gmail.com>
2022-02-24 10:12:51 -0800

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // when button is clicked fuction is called to create new task-item
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 }
 
-// on button click create task
-buttonEl.addEventListener("click", createTaskHandler);
+// on form click create task handler
+formEl.addEventListener("submit", createTaskHandler);
 
-// Lesson 4.2.5
+
+
+// Lesson

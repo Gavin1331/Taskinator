a05c91fe7d709e8e675b7dade5a87d39ed10a7f3
Gavin Jacobsen <gjacobsen1331@gmail.com>
2022-02-24 11:06:21 -0800

    form value display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// when button is clicked fuction is called to create new task-item
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  // add to li
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list (ul)
   tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-// on form click create task handler
 formEl.addEventListener("submit", createTaskHandler);
-
-
-
 // Lesson
